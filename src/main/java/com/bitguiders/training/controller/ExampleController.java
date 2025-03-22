f1a350a47645404cbd90d649d96b11c1d0e92f13
bjt-00 <bitguiders@gmail.com>
2025-03-21 20:30:56 -0400

    OC99:  query parameter example2 added

--- src/main/java/com/bitguiders/training/controller/ExampleController.java
+++ src/main/java/com/bitguiders/training/controller/ExampleController.java
@@ -2,6 +2,7 @@ package com.bitguiders.training.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,11 @@ public class ExampleController {
 	@RequestMapping(value="/training/bat-01/rest/api/example1" , method=RequestMethod.GET)
 	public String stringReturnExample() {
 		return "String return exampe";
+	}
+
+	@RequestMapping(value="/training/bat-01/rest/api/example2" , method=RequestMethod.GET)
+	public String requestParamExample(@RequestParam(name="userName", required=true) String userName) {
+		return "Welcome "+userName;
 	}
 
 }
